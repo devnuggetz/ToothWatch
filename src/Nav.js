ce228d4331c7261ff82c109880231f3b8e53748d
shuvamk <kshuvam564@gmail.com>
2020-09-11 15:04:55 -0700

    Completed Nav Bar

---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+    return (
+        <div className='nav'>
+            <h2>Trending</h2>
+            <h2>Top Rated</h2>
+            <h2>Action</h2>
+            <h2>Horror</h2>
+            <h2>Romance</h2>
+            <h2>Mystery</h2>
+            <h2>Sci-fi</h2>
+            <h2>Thriller</h2>
+            <h2>Drama</h2>
+            <h2>Animated</h2>
+        </div>
+    )
+}
+
+export default Nav
