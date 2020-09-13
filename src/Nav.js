eac1b57d1d3de2998ffa7f5aee3106f4b8d23216
shuvamk <kshuvam564@gmail.com>
2020-09-12 21:44:25 -0700

    Completed whole app and Installed React Flip Move

--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import './Nav.css'
+import requests from './requests'
 
-function Nav() {
+function Nav({setSelectedOption}) {
     return (
         <div className='nav'>
-            <h2>Trending</h2>
-            <h2>Top Rated</h2>
-            <h2>Action</h2>
-            <h2>Horror</h2>
-            <h2>Romance</h2>
-            <h2>Mystery</h2>
-            <h2>Sci-fi</h2>
-            <h2>Thriller</h2>
-            <h2>Drama</h2>
-            <h2>Animated</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchTrending)}>Trending</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchTopRated)}>Top Rated</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchActionMovies)}>Action</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchHorroMovies)}>Horror</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchRomanceMovies)}>Romance</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchMystery)}>Mystery</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchScifi)}>Sci-fi</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchComedyMovies)}>Comedy</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchTV)}>TV</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchAnimation)}>Animation</h2>
+            <h2 onClick={()=> setSelectedOption(requests.fetchWestern)}>Western</h2>
         </div>
     )
 }
--- src/Results.js
+++ src/Results.js
@@ -4,23 +4,24 @@ import VideoCard from './VideoCard'
 import axios from './axios';
 import requests from './requests';
 
-function Results() {
+function Results({ selectedOption }) {
     const [movies, setMovies]= useState([]);
     useEffect(()=>{
         async function fetchData() {
-            const request= await axios.get(requests.fetchActionMovies)
+            const request= await axios.get(selectedOption)
             // console.log(request)
             setMovies(request.data.results)
             return request;
         }
         fetchData();
-    },[])
+    },[selectedOption])
     return (
         <div className='results'>
             {movies.map(movie=>(
                 <VideoCard 
+                key={movie.id}
                 movie={movie}
-                
+
                 />
             ))}
         </div>
